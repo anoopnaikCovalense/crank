3a6e7e226d87555bb484d2604f8f664138900fde
Anoop Naik <anoop@covalense.com>
2018-10-09 11:13:05 +0530

    mail queue wip

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,12 +13,11 @@ class MailController extends Controller
 
     public static function createNewChallenge($challenge)
     {
-        $users = array(User::where('id', '!=', Auth::user()->id)->get(['email', 'name']));
+        $users = User::where('id', '!=', Auth::user()->id)->get(['email', 'name']);
         
         foreach ($users as $user)
         {
-            echo $user[0]->email . PHP_EOL;
-            Mail::to($user[0]->email)->send(new challengeCreated($user, $challenge));
+            Mail::to($user->email)->send(new challengeCreated($user, $challenge));
         }
 
         return true;
--- resources/views/email/challenges/new.blade.php
+++ resources/views/email/challenges/new.blade.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-Hi {{$user[0]->name}}!!
+Hi {{$user->name}}!!
 <h4>A new challenge has been created by {{Auth::user()->name}} . Click the below link to take it.</h4>
 <a href="{{url('challenge?cid=' .$challenge->id )}}" target="_blank">Click Here</a>
 

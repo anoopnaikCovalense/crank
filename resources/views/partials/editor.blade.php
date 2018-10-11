0e200044ae5611ec14c507e3f731748a956391aa
Anoop Naik <anoop@covalense.com>
2018-10-11 16:40:30 +0530

    removing unwanted css

--- resources/views/partials/editor.blade.php
+++ resources/views/partials/editor.blade.php
@@ -2,7 +2,6 @@
 @section('content')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
 <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" />
 <style type="text/css" media="screen">
     body {

5ec7372e1e15986b5bc59f1b1c330a486a86f845
Anoop Naik <anoop@covalense.com>
2018-10-11 17:08:57 +0530

    some design changes

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="padding-top:70px">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header bg-indigo"><span style="color:white">{{ __('Reset Password') }}</span></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -33,7 +33,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-indigo">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>

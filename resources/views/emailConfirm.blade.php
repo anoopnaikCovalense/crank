d7f52fe8784f01fc0bf26e043b50b7fa7ad9022e
anku-gupta <ankit.gupta@covalense.com>
2018-10-12 11:37:55 +0530

    Favicon and Email UI Changes

--- resources/views/emailConfirm.blade.php
+++ resources/views/emailConfirm.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container" style="padding-top:170px">
 <div class="row justify-content-center">
 <div class="col-md-8">
 
 <div class="card">
 
-<div class="card-header">Registration Confirmed</div>
+<div class="card-header bg-indigo"><span style="color:white">Registration Confirmed</span></div>
 <div class="card-body">
-Your Email is successfully verified. Click here to <a href="{{url('/login')}}">login</a>
+Your Email is successfully verified. <br>
+<br>Click here to <a href="{{url('/login')}}">login</a>
 </div>
 </div>
 </div>
--- resources/views/emailVerification.blade.php
+++ resources/views/emailVerification.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-<div class="row">
+<div class="container" style="padding-top:170px">
+<div class="row justify-content-center">
 <div class="col-md-8 col-md-offset-2">
-<div class="panel panel-default">
-<div class="panel-heading">Registration</div>
-<div class="panel-body">
-You have successfully registered. An email is sent to you for verification.
+<div class="card">
+
+<div class="card-header bg-indigo"><span style="color:white">Registration </span></div>
+<div class="card-body">
+You have successfully registered. <br>
+<br> An email is sent to you for verification.
 </div>
 </div>
 </div>

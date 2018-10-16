938e39f43cd75c6179b450d758d075c45d57df5a
KumariAsmita <kumariasmitadhn@gmail.com>
2018-10-16 16:21:54 +0530

    cleanup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
+     
         <style>
             html, body {
                 

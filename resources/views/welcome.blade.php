cdf37253059885d32759113febb4d6d04325c03a
KumariAsmita <kumariasmitadhn@gmail.com>
2018-10-16 16:30:03 +0530

    cleanup in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,14 +85,7 @@
                 </div>
             @endif
 
-                <!-- <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> -->
-            <!-- </div> -->
+                
         </div>
     </body>
 </html>
